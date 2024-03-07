e97a84fc4e2af53bda6b951ae9a5ce592acd7787
super <super@DESKTOP-JBNOU04>
2024-03-07 04:10:49 +0000

    dobj has been improved to allow for up casting from derived to base classes in the move sementic functions using std++20 concepts.

--- main/dobj.cuh
+++ main/dobj.cuh
@@ -3,25 +3,65 @@
 
 #include "cuda_utils.h"
 #include "dmemory.cuh"
+#include <concepts>
+
+template <typename T, typename... Args>
+__global__ void _make_dobj(T* obj_ptr, Args... args) {
+	if (threadIdx.x == 0 && blockIdx.x == 0) {
+		new (obj_ptr) T(args...);
+	}
+}
 
 template <typename T>
+__global__ void _destruct_dobj(T* obj_ptr) {
+	if (threadIdx.x == 0 && blockIdx.x == 0) {
+		obj_ptr->~T();
+	}
+}
+
+template <typename T, bool destructObj = true>
 class dobj {
+	template <typename U, bool destruct>
+	friend class dobj;
+
 	dmemory dmem;
+
+	dobj() : dmem(sizeof(T)) {}
 
 public:
 
-	dobj() = delete;
 	dobj(const dobj&) = delete;
+	dobj& operator=(const dobj&) = delete;
 
-	__host__ dobj(dobj&& other) noexcept : dmem(std::move(other.dmem)) {}
-	__host__ dobj& operator=(dobj&& other) noexcept { dmem = std::move(other.dmem); return *this; }
+	template <typename U = T, bool destruct = true> requires std::derived_from<U, T> && (destruct == destructObj)
+	dobj(dobj<U, destruct>&& other) noexcept : dmem(std::move(other.dmem)) {}
 
-	__host__ dobj(T obj) : dmem(sizeof(T)) {
-		CUDA_ASSERT(cudaMemcpy(dmem.getPtr(), &obj, sizeof(T), cudaMemcpyHostToDevice));
+	template <typename U> requires std::derived_from<U, T>
+	dobj<T>& operator=(dobj<U>&& other) noexcept { dmem = std::move(other.dmem); return *this; }
+
+	~dobj() {
+		if (destructObj) {
+			_destruct_dobj<T><<<1, 1>>>(getPtr());
+			CUDA_ASSERT(cudaDeviceSynchronize());
+		}
 	}
 
-	__host__ __device__ T* getPtr() { return d_obj; }
-	__host__ __device__ const T* getPtr() const { return d_obj; }
+	template <typename... Args>
+	static dobj<T> Make(const Args&... args) {
+		dobj device_obj;
+		_make_dobj<T><<<1, 1>>>(device_obj.getPtr(), args...);
+		CUDA_ASSERT(cudaDeviceSynchronize());
+		return device_obj;
+	}
+	template <typename U = T> requires std::derived_from<U, T>
+	static dobj<T> Copy(const U& obj) {
+		dobj<U> device_obj;
+		CUDA_ASSERT(cudaMemcpy(device_obj.getPtr(), &obj, sizeof(U), cudaMemcpyHostToDevice));
+		return device_obj;
+	}
+
+	T* getPtr() { return dmem.getPtr<T>(); }
+	const T* getPtr() const { return dmem.getPtr<T>(); }
 };
 
 #endif // DEVICE_OBJECT_CLASS_H //
