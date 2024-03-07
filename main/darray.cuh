ed63473a301aa6b46b1f0b45981e4a6b4533a11e
super <super@DESKTOP-JBNOU04>
2024-03-07 04:27:03 +0000

    dmemory functions are now stricly host side. Has implemented a type integrated option that will call the destructor on its data if selected.

--- main/darray.cuh
+++ main/darray.cuh
@@ -6,6 +6,14 @@
 #include <vector>
 
 template <typename T>
+__global__ void _destruct_objs(T* objs_ptr, size_t length) {
+	int gid = threadIdx.x * blockIdx.x * blockDim.x;
+	if (gid >= length) return;
+
+	objs_ptr[gid].~T();
+}
+
+template <typename T, bool destructObj = false>
 class darray {
 	dmemory dmem;
 
@@ -13,21 +21,22 @@ public:
 
 	darray() = delete;
 	darray(const darray&) = delete;
+	darray& operator=(const darray&) = delete;
 
-	__host__ darray(darray&& other) noexcept : dmem(std::move(other.dmem)) {}
-	__host__ darray& operator=(darray&& other) noexcept { dmem = std::move(other.dmem) return *this; }
+	darray(darray&& other) noexcept : dmem(std::move(other.dmem)) {}
+	darray& operator=(darray&& other) noexcept { dmem = std::move(other.dmem); return *this; }
 
-	__host__ darray(size_t size) : dmem(size * sizeof(T)) {}
-	__host__ darray(const std::vector<const T>& v) : darray(v.data(), v.size()) {}
-	__host__ darray(const T* arr, size_t size) : darray(size * sizeof(T)) {
+	darray(size_t size) : dmem(size * sizeof(T)) {}
+	darray(const T* arr, size_t size) : darray(size * sizeof(T)) {
 		CUDA_ASSERT(cudaMemcpy(dmem.getPtr(), arr, size * sizeof(T), cudaMemcpyHostToDevice));
 	}
+	darray(const std::vector<T>& v) : darray(v.data(), v.size()) {}
 
-	__host__ __device__ T* getPtr() noexcept { return dmem.getPtr<T>(); }
-	__host__ __device__ const T* getPtr() const noexcept { return dmem.getPtr<T>(); }
+	T* getPtr() noexcept { return dmem.getPtr<T>(); }
+	const T* getPtr() const noexcept { return dmem.getPtr<T>(); }
 
-	T& operator=(size_t i) noexcept { return getPtr()[i]; }
-	const T& operator=(size_t i) const noexcept { return getPtr()[i]; }
+	T* operator=(size_t i) noexcept { return getPtr() + i; }
+	const T* operator=(size_t i) const noexcept { return getPtr() + i; }
 };
 
 #endif // DEVICE_ARRAY_CLASS_H //
