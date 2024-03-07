dedc206b03e6160f4e992d4d9a5b5852d5c77679
super <super@DESKTOP-JBNOU04>
2024-03-07 04:06:40 +0000

    Removed __host__ and __device__ notation on dmemory functions making it strictly host side only. Also removed templated constructor as it is inaccessable.

--- main/dmemory.cuh
+++ main/dmemory.cuh
@@ -6,7 +6,7 @@
 class dmemory {
 	void* device_memory;
 
-	__host__ void _free() {
+	void _free() {
 		if (device_memory)
 			CUDA_ASSERT(cudaFree(device_memory));
 	}
@@ -17,11 +17,11 @@ public:
 	dmemory(const dmemory&) = delete;
 	dmemory& operator=(const dmemory&) = delete;
 
-	__host__ dmemory(dmemory&& other) noexcept {
+	dmemory(dmemory&& other) noexcept {
 		device_memory = other.device_memory;
 		other.device_memory = nullptr;
 	}
-	__host__ dmemory& operator=(dmemory&& other) noexcept {
+	dmemory& operator=(dmemory&& other) noexcept {
 		_free();
 
 		device_memory = other.device_memory;
@@ -30,20 +30,18 @@ public:
 		return *this;
 	}
 
-	template <typename T>
-	__host__ dmemory(size_t size) : dmemory(sizeof(T) * size) {}
-	__host__ dmemory(size_t size) {
+	dmemory(size_t size) {
 		CUDA_ASSERT(cudaMalloc(&device_memory, size));
 	}
-	__host__ ~dmemory() {
+	~dmemory() {
 		_free();
 	}
 
-	__host__ __device__ template <typename T> const T* getPtr() const noexcept { return static_cast<const T*>(getPtr()); }
-	__host__ __device__ template <typename T> T* getPtr() noexcept { return static_cast<T*>(getPtr()); }
+	const void* getPtr() const noexcept { return device_memory; }
+	void* getPtr() noexcept { return device_memory; }
 
-	__host__ __device__ const void* getPtr() const noexcept { return device_memory; }
-	__host__ __device__ void* getPtr() noexcept { return device_memory; }
+	template <typename T> const T* getPtr() const noexcept { return static_cast<const T*>(getPtr()); }
+	template <typename T> T* getPtr() noexcept { return static_cast<T*>(getPtr()); }
 };
 
 #endif // DEVICE_MEMORY_CLASS_H //
