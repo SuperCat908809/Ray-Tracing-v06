fd214a363023094ca2c1f8a099938faf1170ea24
super <super@DESKTOP-JBNOU04>
2024-03-08 02:50:36 +0000

    cuda_utils.h has been re-vamped to improve the cuda error checking by giving three options on how to respond to an error detection: assertion failure, std::runtime_error thrown or return a boolean, true if error detected and false otherwise so the caller can deal with it without an expensive error throw for performance.

--- main/cuda_utils.h
+++ main/cuda_utils.h
@@ -1,12 +1,16 @@
 #ifndef CUDA_UTILITIES_H
 #define CUDA_UTILITIES_H
 
+#include <string>
+#include <format>
 #include <assert.h>
 #include <stdlib.h>
-#include <vector>
+#include <iostream>
 #include <stdexcept>
+
 #include <cuda_runtime.h>
 
+#if 0
 #define CUDA_CHECK(func) cudaAssert(func, #func, __FILE__, __LINE__)
 #define CUDA_ASSERT(func) try { CUDA_CHECK(func); } catch (const std::runtime_error&) { assert(0); }
 inline void cudaAssert(cudaError_t code, const char* func, const char* file, const int line) {
@@ -19,5 +23,25 @@ inline void cudaAssert(cudaError_t code, const char* func, const char* file, con
 		throw std::runtime_error(cudaGetErrorString(code));
 	}
 }
+#else
+
+inline bool cuIsError(cudaError_t code) { return code != cudaSuccess; }
+inline std::string cuFormatErrorMessage(cudaError_t code, const char* func, const char* file, int line) {
+	return std::format("GPU assert : {} at {}\n{} {}\n{} :: {}",
+		cudaGetErrorName(code), func,
+		file, line,
+		cudaGetErrorName(code), cudaGetErrorString(code));
+}
+
+#define GET_ERR_MSG(func, code) cuFormatErrorMessage(code, #func, __FILE__, __LINE__)
+#define CUDA_THROW(func) { cudaError_t _err_code = func; if (cuIsError(_err_code)) { throw std::runtime_error(GET_ERR_MSG(func, _err_code)); } }
+#define CUDA_CHECK(func) cuIsError(func)
+#ifdef NDEBUG
+#define CUDA_ASSERT(func) { func; }
+#else
+#define CUDA_ASSERT(func) { cudaError_t _err_code = func; if (cuIsError(_err_code)) { std::cerr << GET_ERR_MSG(func, _err_code); assert(0); } }
+#endif
+
+#endif
 
 #endif // CUDA_UTILITIES_H //
