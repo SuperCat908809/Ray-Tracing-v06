4c72472be923b289a32eee52193d07cb8fcf5378
super <super@DESKTOP-JBNOU04>
2024-03-08 02:42:58 +0000

    darray now deconstructs its objects when specified using the type declaration. darray also holds a length parameter which can be publicly read.

--- main/darray.cuh
+++ main/darray.cuh
@@ -13,9 +13,10 @@ __global__ void _destruct_objs(T* objs_ptr, size_t length) {
 	objs_ptr[gid].~T();
 }
 
-template <typename T, bool destructObj = false>
+template <typename T, bool destruct = false>
 class darray {
 	dmemory dmem;
+	size_t length;
 
 public:
 
@@ -23,20 +24,31 @@ public:
 	darray(const darray&) = delete;
 	darray& operator=(const darray&) = delete;
 
-	darray(darray&& other) noexcept : dmem(std::move(other.dmem)) {}
-	darray& operator=(darray&& other) noexcept { dmem = std::move(other.dmem); return *this; }
+	darray(darray&& other) noexcept : length(other.length), dmem(std::move(other.dmem)) {}
+	darray& operator=(darray&& other) noexcept { length = other.length; dmem = std::move(other.dmem); return *this; }
 
-	darray(size_t size) : dmem(size * sizeof(T)) {}
-	darray(const T* arr, size_t size) : darray(size * sizeof(T)) {
-		CUDA_ASSERT(cudaMemcpy(dmem.getPtr(), arr, size * sizeof(T), cudaMemcpyHostToDevice));
+	darray(size_t length) : length(length), dmem(length * sizeof(T)) {}
+	darray(const T* arr, size_t length) : length(length), dmem(length * sizeof(T)) {
+		CUDA_ASSERT(cudaMemcpy(dmem.getPtr(), arr, length * sizeof(T), cudaMemcpyHostToDevice));
 	}
 	darray(const std::vector<T>& v) : darray(v.data(), v.size()) {}
+
+	~darray() {
+		if (destruct) {
+			int threads = 32;
+			int blocks = ceilDiv(length, threads);
+			_destruct_objs<T><<<blocks, threads>>>(getPtr(), length);
+			CUDA_ASSERT(cudaDeviceSynchronize());
+		}
+	}
+
+	size_t getLength() const { return length; }
 
 	T* getPtr() noexcept { return dmem.getPtr<T>(); }
 	const T* getPtr() const noexcept { return dmem.getPtr<T>(); }
 
-	T* operator=(size_t i) noexcept { return getPtr() + i; }
-	const T* operator=(size_t i) const noexcept { return getPtr() + i; }
+	T* operator[](size_t i) noexcept { return getPtr() + i; }
+	const T* operator[](size_t i) const noexcept { return getPtr() + i; }
 };
 
 #endif // DEVICE_ARRAY_CLASS_H //
