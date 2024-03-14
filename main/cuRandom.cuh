2e038f5061340adc9f438bed94fc3f5cb235033e
super <super@DESKTOP-JBNOU04>
2024-03-14 12:50:34 +0000

    just keeping is simple and making cuRandom just a wrapper to the curand_uniform function with support for making a vector of any length. Other distributions and generators can be implemented as of when they are needed.

--- main/cuRandom.cuh
+++ main/cuRandom.cuh
@@ -29,20 +29,8 @@ public:
 };
 #else
 
-enum DistributionType { UNIFORM, NORMAL, LOG_NORMAL, POISSON, DEFAULT = UNIFORM };
 class cuRandom {
 	curandStateXORWOW_t gen;
-
-	template <DistributionType D, typename T> __device__ void _RND(T& r1) = 0;
-	template <DistributionType D, typename T> __device__ void _RND(T& r1, T& r2) = 0;
-	template <DistributionType D, typename T> __device__ void _RND(T& r1, T& r2, T& r3, T& r4) = 0;
-
-	template <DistributionType D, glm::length_t L, typename T, glm::qualifier Q>
-	__device__ void _RND4R(glm::vec<L, T, Q>& v) {
-		for (int i = 0; i < 4; i += 4) {
-			_RND<D, T>(v[i + 0], v[i + 1], v[i + 2], v[i + 3]);
-		}
-	}
 
 public:
 
@@ -50,39 +38,27 @@ public:
 		curand_init(seed, sequence, offset, &gen);
 	}
 
-	template <typename ret_type> __device__ ret_type nextUniform();
-	template <typename ret_type> __device__ ret_type nextNormal();
-	template <typename ret_type> __device__ ret_type nextLogNormal(float mean, float stddev);
-	template <typename ret_type> __device__ ret_type nextPoisson(float lambda);
+	__device__ float next() { return curand_uniform(&gen); }
 
-	template <DistributionType D, glm::length_t L, typename T, glm::qualifier Q = glm::qualifier::packed_highp>
-	__device__ glm::vec<L, T, Q> nextUniformVector() {
-		glm::vec<L, T, Q> v;
-		if constexpr (L >= 4) {
-			_RND4R<D, L, T, Q>(v);
+	template <glm::length_t L>
+	__device__ glm::vec<L, float, glm::qualifier::packed_highp> next() {
+		glm::vec<L, float, glm::qualifier::packed_highp> v;
+		if constexpr (L <= 4) {
+			if constexpr (1 <= L) v[1] = curand_uniform(&gen);
+			if constexpr (2 <= L) v[2] = curand_uniform(&gen);
+			if constexpr (3 <= L) v[3] = curand_uniform(&gen);
+			if constexpr (4 <= L) v[4] = curand_uniform(&gen);
 		}
-		if constexpr (L & 0b11) {
-			const glm::length_t i = L & !0b11;
-			if constexpr (i + 1 < L) _RND<D, T>(v[i + 1]);
-			if constexpr (i + 2 < L) _RND<D, T>(v[i + 2]);
-			if constexpr (i + 3 < L) _RND<D, T>(v[i + 3]);
+		else constexpr {
+			for (int i = 0; i < L; i++) {
+				v[i] = curand_uniform(&gen);
+			}
 		}
 		return v;
 	}
 
 	__device__ void Skipahead(uint64_t n) { skipahead(n, &gen); }
 };
-
-template <> __device__ float cuRandom::  nextUniform()							{ return curand_uniform(&gen); }
-template <> __device__ float cuRandom::   nextNormal()							{ return curand_normal(&gen); }
-template <> __device__ float cuRandom::nextLogNormal(float mean, float stddev)	{ return curand_log_normal(&gen, mean, stddev); }
-template <> __device__ float cuRandom::  nextPoisson(float lambda)				{ return curand_poisson(&gen, lambda); }
-
-
-template <> __device__ void cuRandom::_RND<UNIFORM, float>(float& r1) { r1 = curand_uniform(&gen); }
-template <> __device__ void cuRandom::_RND<UNIFORM, float>(float& r1, float& r2) { r1 = curand_uniform(&gen); r2 = curand_uniform(&gen); }
-//template <> __device__ void cuRandom::_RND<UNIFORM, float>(float& r1, float& r2, float& r3, float& r4) { float4 R = curand_uniform4(&gen); }
-template <> __device__ void cuRandom::_RND<NORMAL, float>(float& r1) { r1 = curand_normal(&gen); }
 
 #endif
 
