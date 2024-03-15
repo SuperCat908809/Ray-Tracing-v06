5538f9d64d3efe3e3f0465617b29e1057c10d3f3
super <super@DESKTOP-JBNOU04>
2024-03-15 14:59:20 +0000

    dabbled a bit more with concepts. i dont think ill keep this but its a stepping stone to be better solution

--- main/cuAllocators.cuh
+++ main/cuAllocators.cuh
@@ -12,4 +12,31 @@ concept cuAllocator = requires (_cuAlloc_t&& a, _cuAlloc_t && b, size_t count, T
 	{ a.deallocate(to_be_deallocated) };
 };
 
+
+template <class _cuManager_t, typename T, typename... Args>
+concept cuManager_c = requires (_cuManager_t& a, T* ptr, Args&&... args) {
+	{ a.make_at(ptr, args...) } -> std::same_as<T*>;
+	{ a.destruct(ptr) };
+};
+
+template <class _cuManager_t, typename T, typename... Args>
+concept cuManager_constructor_c = requires (_cuManager_t& a, T* ptr, Args&&... args) {
+	{ a.construct_at(ptr, std::forward<Args>(args)...) };
+};
+
+template <class _cuManager_t, typename T>
+concept cuManager_destructor_c = requires (_cuManager_t& a, T* ptr) {
+	{ a.destruct(ptr) };
+};
+
+template <class _cuManager_t, typename T>
+concept cuManager_move_c = requires (_cuManager_t& a, T* dst, T* src) {
+	{ a.move_to(dst, src) };
+};
+
+template <class _cuManager_t, typename T>
+concept cuManager_copy_c = requires (_cuManager_t& a, T* dst, T* src) {
+	{ a.copy_to(dst, src) };
+}
+
 #endif // CUDA_ALLOCATOR_ABSTRACT_CUH //
