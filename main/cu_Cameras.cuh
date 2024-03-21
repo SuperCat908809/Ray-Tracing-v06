6e8401052d338a98615f8b0f6b523edd82681111
SuperCat 908809 <supercat908809@gmail.com>
2024-03-21 02:50:28 +0000

    added another camera class to model defocus blur in non-pinhole cameras

--- main/cu_Cameras.cuh
+++ main/cu_Cameras.cuh
@@ -4,7 +4,9 @@
 #include <math.h>
 #include <glm/glm.hpp>
 #include <cuda_runtime.h>
+#include "cuRandom.cuh"
 #include "ray_data.cuh"
+#include "glm_utils.h"
 
 
 struct PinholeCamera {
@@ -25,6 +27,39 @@ struct PinholeCamera {
 	__host__ __device__ Ray sample_ray(float s, float t) const {
 		Ray ray(o, w + u * s + v * t);
 		return ray;
+	}
+};
+
+struct DefocusBlurCamera {
+	glm::vec3 o{}, u{}, v{}, w{};
+	float viewport_width{}, viewport_height{};
+	float lens_radius{}, focus_dist{};
+
+	__host__ __device__ DefocusBlurCamera() {}
+	__host__ __device__ DefocusBlurCamera(glm::vec3 lookfrom, glm::vec3 lookat, glm::vec3 up, float vfov, float aspect_ratio, float aperture, float focus_dist) {
+		float theta = glm::radians(vfov);
+		viewport_height = tanf(theta * 0.5f);
+		viewport_width = viewport_height * aspect_ratio;
+
+		o = lookfrom;
+		w = glm::normalize(lookat - lookfrom);
+		u = glm::normalize(glm::cross(up, w));
+		v = glm::normalize(glm::cross(w, u));
+
+		lens_radius = aperture * 0.5f;
+		this->focus_dist = focus_dist;
+	}
+
+	__device__ Ray sample_ray(float s, float t, cuRandom& rnd) const {
+		glm::vec2 disc = glm::cuRandomInUnit<2>(rnd);
+		glm::vec3 offset = u * disc.x + v * disc.y;
+		offset *= lens_radius;
+
+		glm::vec3 forward = w * focus_dist;
+		glm::vec3 hori = u * viewport_width * focus_dist;
+		glm::vec3 vert = v * viewport_height * focus_dist;
+
+		return Ray(o + offset, forward + hori * s + vert * t - offset);
 	}
 };
 
