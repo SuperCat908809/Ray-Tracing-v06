0db38efab353f56951c17c6824beaaa856d3ebcd
SuperCat 908809 <supercat908809@gmail.com>
2024-03-21 20:05:10 +0000

    added another camera class, MotionBlurCamera, that is capable of generating rays over a period of time

--- main/cu_Cameras.cuh
+++ main/cu_Cameras.cuh
@@ -30,6 +30,7 @@ struct PinholeCamera {
 	}
 };
 
+
 struct DefocusBlurCamera {
 	glm::vec3 o{}, u{}, v{}, w{};
 	float viewport_width{}, viewport_height{};
@@ -60,6 +61,31 @@ struct DefocusBlurCamera {
 		glm::vec3 vert = v * viewport_height * focus_dist;
 
 		return Ray(o + offset, forward + hori * s + vert * t - offset);
+	}
+};
+
+
+struct MotionBlurCamera {
+	glm::vec3 o, u, v, w;
+	float t0, t1;
+
+	__host__ __device__ MotionBlurCamera() : o(), u(), v(), w(), t0(0.0f), t1(1.0f) {}
+	__host__ __device__ MotionBlurCamera(glm::vec3 lookfrom, glm::vec3 lookat, glm::vec3 up, float vfov, float aspect_ratio, float time0, float time1) {
+		t0 = time0;
+		t1 = time1;
+
+		float theta = glm::radians(vfov);
+		float viewport_height = tanf(theta * 0.5f);
+		float viewport_width = viewport_height * aspect_ratio;
+
+		o = lookfrom;
+		w = glm::normalize(lookat - lookfrom);
+		u = glm::normalize(glm::cross(up, w)) * viewport_width;
+		v = glm::normalize(glm::cross(w, u)) * viewport_height;
+	}
+
+	__device__ Ray sample_ray(float s, float t, cuRandom& rnd) const {
+		return Ray(o, w + u * s + v * t, glm::mix(t0, t1, rnd.next()));
 	}
 };
 
