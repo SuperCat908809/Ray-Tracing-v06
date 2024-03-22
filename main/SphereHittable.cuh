a8d6ecfe8c784760f1484bfe823a8c8b5fb4f1ee
super <super@DESKTOP-JBNOU04>
2024-03-22 14:20:22 +0000

    another brain storm on how to solve the AABB issue

--- main/SphereHittable.cuh
+++ main/SphereHittable.cuh
@@ -96,6 +96,14 @@ Perhaps I can re-work everything such that during construction, like through a f
 then whenever I need something like its bounding box then I can easily access it and once everything is done host-side, every device side object will be constructed and 
 all the necessary pointer passing will occur such that everything points to the correct data.
 
+
+Hittable should only provide the interface for intersectable geometry and doesn't have the responsibility of returning the AABB.
+It should be up to the host to provide the AABB and any other data not related directly to rendering.
+Generating an AABB should be in the scene construction step so the host should store whatever it needs, either a full copy of the Hittable or a pointer to fetch it if required, 
+which will be used to provide the requested data (AABB and such).
+
+Perhaps I can make a second abstract class that provides these accessors.
+
 */
 
 
