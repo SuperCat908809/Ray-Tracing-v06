51784b2a952d1281d2557a7448c8a4f812a19823
super <super@DESKTOP-JBNOU04>
2024-03-22 19:08:27 +0000

    fixed aabb to use glm's built in component wise min, max and now use the glm min, max functions instead of mistakenly using std's min, max functions

--- main/aabb.cuh
+++ main/aabb.cuh
@@ -3,6 +3,7 @@
 
 #include <cuda_runtime.h>
 #include <glm/glm.hpp>
+#include <glm/gtx/component_wise.hpp>
 #include "glm_utils.h"
 #include "ray_data.cuh"
 
@@ -11,9 +12,16 @@ class aabb {
 	glm::vec3 min, max;
 
 public:
-	__host__ __device__ aabb() : min(0), max(0) {}
+	// float maximum in min and float minimum in max guarantees that all rays will 'miss' this aabb and guarantees that 
+	// expanding this aabb by another will equal the other, effectively initialising it.
+	__host__ __device__ aabb() : min(3.402823466e+38F), max(-3.402823466e+38F) {}
 	__host__ __device__ aabb(glm::vec3 min, glm::vec3 max) : min(glm::min(min, max)), max(glm::max(min, max)) {}
 	__host__ __device__ aabb(const aabb& a, const aabb& b) : min(glm::min(a.min, b.min)), max(glm::max(a.max, b.max)) {}
+
+	__host__ __device__ glm::vec3 getMin() const { return min; }
+	__host__ __device__ glm::vec3 getMax() const { return max; }
+
+	__host__ __device__ aabb& operator+=(const aabb& bounds) { min = glm::min(min, bounds.min); max = glm::max(max, bounds.max); return *this; }
 
 	__host__ __device__ bool intersects(const Ray& ray, float t_max) const {
 		glm::vec3 bmin = (min - ray.o) / ray.d;
@@ -23,8 +31,8 @@ public:
 		bmax = glm::max(bmin, bmax);
 		bmin = tmp;
 
-		float tmin = glm::compwise_max(bmin);
-		float tmax = glm::compwise_min(bmax);
+		float tmin = glm::compMax(bmin);
+		float tmax = glm::compMin(bmax);
 
 		return tmax >= tmin && tmin < t_max && tmax > 0;
 	}
