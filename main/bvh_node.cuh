a6a4073763b4087ed3762610174d5b2bba064eaf
SuperCat 908809 <supercat908809@gmail.com>
2024-03-23 04:59:39 +0000

    threw together a quick bvh_node class to follow the ebook. construction will happen on the host and it is up to the user to provide a valid hierarchy

---
+++ main/bvh_node.cuh
@@ -0,0 +1,27 @@
+#ifndef BVH_NODE_CLASS_CUH
+#define BVH_NODE_CLASS_CUH
+
+#include <cuda_runtime.h>
+#include "hittable.cuh"
+#include "aabb.cuh"
+
+
+class bvh_node : public Hittable {
+	aabb bounds;
+	Hittable* left;
+	Hittable* right;
+
+public:
+
+	__device__ bvh_node() : left(nullptr), right(nullptr), bounds() {}
+	__device__ bvh_node(Hittable* left, Hittable* right, const aabb& bounds) : left(left), right(right), bounds(bounds) {}
+
+	__device__ virtual bool ClosestIntersection(const Ray& ray, TraceRecord& rec) const override {
+		if (!bounds.intersects(ray, rec.t)) return false;
+		bool hit = left->ClosestIntersection(ray, rec);
+		hit |= right->ClosestIntersection(ray, rec);
+		return hit;
+	}
+};
+
+#endif // BVH_NODE_CLASS_CUH //
