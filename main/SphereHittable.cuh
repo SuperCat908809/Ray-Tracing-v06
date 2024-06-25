ae46762d1417cae9260119b5b49cb4e144a4321c
SuperCat 908809 <supercat908809@gmail.com>
2024-06-25 11:12:07 +0100

    Removed old sphere classes and yappage notes

--- main/SphereHittable.cuh
+++ main/SphereHittable.cuh
@@ -25,50 +25,7 @@ __device__ inline float _sphere_closest_intersection(const Ray& ray, glm::vec3 c
 	}
 
 	return t;
-
-#if 0
-	rec.t = t;
-	glm::vec3 normal = (ray.at(rec.t) - sp.center) / sp.radius; // a negative radius will flip the normal as intended
-	rec.set_face_normal(ray, normal);
-	return true;
-#endif
 }
-
-#if 0
-class Sphere {
-	glm::vec3 origin{ 0,0,0 };
-	float radius{ 1 };
-	Material* mat_ptr{ nullptr };
-
-public:
-
-	__host__ __device__ Sphere(glm::vec3 origin, float radius, Material* mat_ptr) : origin(origin), radius(radius), mat_ptr(mat_ptr) {}
-
-	__device__ bool ClosestIntersection(const Ray& ray, TraceRecord& rec) const {
-		glm::vec3 oc = ray.o - origin;
-
-		float a = glm::dot(ray.d, ray.d);
-		float hb = glm::dot(ray.d, oc);
-		float c = glm::dot(oc, oc) - radius * radius;
-		float d = hb * hb - a * c;
-		if (d <= 0) return false;
-
-		d = sqrtf(d);
-		float t = (-hb - d) / a;
-		if (t < 0.0f || t > rec.t) {
-			t = (-hb + d) / a;
-			if (t < 0.0f || t > rec.t)
-				return false;
-		}
-
-		rec.t = t;
-		glm::vec3 normal = (ray.at(rec.t) - origin) / radius; // a negative radius will flip the normal as intended
-		rec.set_face_normal(ray, normal);
-		rec.mat_ptr = mat_ptr;
-		return true;
-	}
-};
-#endif
 
 
 class Sphere : public Geometry {
@@ -113,6 +70,7 @@ public:
 
 	template <typename MatType> requires GeoAcceptable<Sphere, MatType>
 	__device__ SphereHittable(const Sphere* sphere, const MatType* mat_ptr) : sphere(sphere), mat_ptr(mat_ptr) {}
+
 	__device__ virtual bool ClosestIntersection(const Ray& ray, RayPayload& rec) const override {
 		float t = _sphere_closest_intersection(ray, sphere->center, sphere->radius);
 		if (t >= rec.distance) return false;
@@ -125,25 +83,6 @@ public:
 		return true;
 	}
 };
-
-
-
-/*
-
-I realise that constructing a bvh hierarchy must be done on the device, thus making the bounding_box function only accessible on the device is a problem.
-Perhaps I can re-work everything such that during construction, like through a factory object, all the data pertaining to be device side object is available on the host, 
-then whenever I need something like its bounding box then I can easily access it and once everything is done host-side, every device side object will be constructed and 
-all the necessary pointer passing will occur such that everything points to the correct data.
-
-
-Hittable should only provide the interface for intersectable geometry and doesn't have the responsibility of returning the AABB.
-It should be up to the host to provide the AABB and any other data not related directly to rendering.
-Generating an AABB should be in the scene construction step so the host should store whatever it needs, either a full copy of the Hittable or a pointer to fetch it if required, 
-which will be used to provide the requested data (AABB and such).
-
-Perhaps I can make a second abstract class that provides these accessors, specifically for host side code.
-
-*/
 
 
 class MovingSphere : public Geometry {
@@ -192,6 +131,7 @@ public:
 
 	template <typename MatType> requires GeoAcceptable<MovingSphere, MatType>
 	__device__ MovingSphereHittable(const MovingSphere* moving_sphere, const MatType* mat_ptr) : moving_sphere(moving_sphere), mat_ptr(mat_ptr) {}
+
 	__device__ virtual bool ClosestIntersection(const Ray& ray, RayPayload& rec) const override {
 		glm::vec3 center = glm::mix(moving_sphere->center0, moving_sphere->center1, ray.time);
 		float t = _sphere_closest_intersection(ray, center, moving_sphere->radius);
