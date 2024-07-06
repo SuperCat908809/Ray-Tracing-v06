0722737735088698a5dadf9a8fa5c71ce33cdb6f
SuperCat 908809 <supercat908809@gmail.com>
2024-07-06 23:47:19 +0100

    added starter boilerplate code for glfw and opengl init

--- main/src/main.cu
+++ main/src/main.cu
@@ -9,7 +9,7 @@
 #include "FirstApp.cuh"
 
 
-int main() {
+int nmain() {
 
 	{
 		printf("Creating FirstApp object...\n");
@@ -26,4 +26,44 @@ int main() {
 	printf("\n\nFinished.\n");
 
 	return 0;
+}
+
+
+
+
+
+
+int main() {
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Window test", nullptr, nullptr);
+	if (window == nullptr) {
+		printf("Failed to create window.\n");
+		glfwTerminate();
+		CUDA_ASSERT(cudaDeviceReset());
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+
+	while (!glfwWindowShouldClose(window)) {
+
+
+
+	}
+
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	CUDA_ASSERT(cudaDeviceReset());
+
+	printf("\n\nFinished.\n");
+
+	return 0;
+
 }
